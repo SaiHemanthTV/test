33f6578a71f1c924eca17fd5088e3d1504909407
SaiHemanthTV <47324880+SaiHemanthTV@users.noreply.github.com>
2024-03-18 12:29:15 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+WEBSITE="http://3.85.147.17:8080/testenv/"  # Replace with the URL you want to check
+
+# Make a request to the website and retrieve the HTTP status code
+HTTP_STATUS=$(curl -Is "$WEBSITE" | head -1 | cut -d' ' -f2)
+
+if [ "$HTTP_STATUS" = "200" ]; then
+    echo "$WEBSITE is up!"
+else
+    echo "$WEBSITE is down."
+fi
